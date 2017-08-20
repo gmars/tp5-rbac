d4935b9eaeb5ca59d85dbeb910c69ec9d1fd64a6
weiyongqiang <hayixia606@163.com>
2017-08-21 01:53:15 +0800

    路径修复

---
+++ src/Rbac.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: 魏永强   <hayixia606@163.com>
+ * GitHub: https://github.com/gmars
+ * Blog: http://blog.csdn.net/marswill
+ * Date: 2017/8/21
+ * Time: 上午12:38
+ */
+
+namespace gmars\rbac;
+
+
+class Rbac
+{
+
+}
